a58c1f959822ad3d43dc2ad31d94a80ee61b538c
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2019-07-24 22:53:09 +0530

    Update listener

--- husky_highlevel_controller/src/listener.py
+++ husky_highlevel_controller/src/listener.py
@@ -2,22 +2,22 @@
 
 import rospy
 import os
-# from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 
 
 def callback(msg):
-    os.system('clear')
-    rospy.loginfo(" Distance: {}\n"
-                  .format(msg.ranges[len(msg.ranges) / 2]))
-
+    print len(msg.ranges)
 
 def listener():
     rospy.init_node('listener')
-    topicName = rospy.get_param('~topic')
+    topicname = rospy.get_param('~topic')
     queueSize = rospy.get_param('~queue')
-    rospy.Subscriber(name=topicName, data_class=LaserScan,
-                     callback=callback, queue_size=queueSize)
+    rospy.Subscriber(
+                    name=topicname,
+                    data_class=LaserScan,
+                    callback=callback,
+                    queue_size=queueSize
+                    )
     rospy.spin()
 
 
